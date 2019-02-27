55deb60dbc558cb580d6398e8955b65a6c218b91
Inuka Shanesh de Costha <inuka@iShanesh-Mac.local>
2019-02-27 13:48:45 +0100

    super hero app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,17 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' , desc: 'Consectetur consectetur sint voluptate proident nulla esse.' },
+  { id: 12, name: 'Narco' , desc: 'Aute proident sint minim irure veniam amet velit eu occaecat pariatur velit voluptate deserunt.'},
+  { id: 13, name: 'Bombasto' , desc: 'Sit eu sint labore sunt sit reprehenderit.'},
+  { id: 14, name: 'Celeritas' , desc: 'Ipsum aute consequat non nisi dolore dolor laborum.'}];
